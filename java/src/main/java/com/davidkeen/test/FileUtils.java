0b5ad89a7beacb503f9c7af12ad92b1647303bce
David Keen <david@davidkeen.com>
2017-06-09 16:20:43 +0100

    Read file contents using java.nio

--- java/src/main/java/com/davidkeen/test/FileUtils.java
+++ java/src/main/java/com/davidkeen/test/FileUtils.java
@@ -1,14 +1,24 @@
 package com.davidkeen.test;
 
-import com.google.common.io.Files;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class FileUtils {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(readFileFromClasspathSpring("test.txt"));
+
+        // Using the class's class loader means we need to use an absolute path here.
+        System.out.println(readFileFromClasspathJava("/test.txt"));
+    }
 
     /**
      * Uses Spring Resource interface and Guava Files to read a file from the classpath into a String
@@ -16,10 +26,20 @@ public class FileUtils {
      *
      * @param fileName the name of the file to read.
      * @return String contents of the file.
-     * @throws java.io.IOException
      */
-    public static String readFileFromClasspath(String fileName) throws IOException {
+    public static String readFileFromClasspathSpring(String fileName) throws IOException {
         Resource resource = new ClassPathResource(fileName);
-        return Files.toString(resource.getFile(), Charset.defaultCharset());
+        return com.google.common.io.Files.toString(resource.getFile(), Charset.defaultCharset());
+    }
+
+    /**
+     * Uses {@link java.nio.file.Files} and {@link java.nio.file.Paths} to read a file from the classpath.
+     *
+     * @param fileName the absolute path to the file.
+     * @return String contents of the file.
+     */
+    public static String readFileFromClasspathJava(String fileName) throws URISyntaxException, IOException {
+        Path path = Paths.get(FileUtils.class.getResource(fileName).toURI());
+        return new String(java.nio.file.Files.readAllBytes(path), StandardCharsets.UTF_8);
     }
 }
