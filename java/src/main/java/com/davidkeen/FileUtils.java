d80a2c5b4c7c00682674c2f7f3b1d608882a4edd
David Keen <david@davidkeen.com>
2017-11-20 16:58:45 +0000

    Add line processing example

--- java/src/main/java/com/davidkeen/FileUtils.java
+++ java/src/main/java/com/davidkeen/FileUtils.java
@@ -3,10 +3,12 @@ package com.davidkeen;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -16,8 +18,14 @@ public class FileUtils {
     public static void main(String[] args) throws Exception {
         System.out.println(readFileFromClasspathSpring("test.txt"));
 
+        System.out.println("----");
+
         // Using the class's class loader means we need to use an absolute path here.
         System.out.println(readFileFromClasspathJava("/test.txt"));
+
+        System.out.println("----");
+
+        reverseLinesFromFile("/test.txt");
     }
 
     /**
@@ -41,5 +49,22 @@ public class FileUtils {
     public static String readFileFromClasspathJava(String fileName) throws URISyntaxException, IOException {
         Path path = Paths.get(FileUtils.class.getResource(fileName).toURI());
         return new String(java.nio.file.Files.readAllBytes(path), StandardCharsets.UTF_8);
+    }
+
+    /**
+     * Uses the {@link BufferedReader#lines()} method to print each line in a file from the classpath.
+     *
+     * @param fileName the absolute path to the file.
+     */
+    public static void reverseLinesFromFile(String fileName) throws URISyntaxException, IOException {
+        Path path = Paths.get(FileUtils.class.getResource(fileName).toURI());
+        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            reader.lines()
+                    .map(line -> {
+                        StringBuilder sb = new StringBuilder(line);
+                        return sb.reverse();
+                    })
+                    .forEach(System.out::println);
+        }
     }
 }
